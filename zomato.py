445ad3d566091324a5ed5a23ebf1a2ecb81f0539
ABHISHEK MALVIYA <122380791+ABHISHEK-ALGO@users.noreply.github.com>
2023-10-14 15:20:57 +0530

    data preprocess

---
+++ zomato.py
@@ -0,0 +1,16 @@
+ Volume in drive G is New Volume
+ Volume Serial Number is 5C21-FE27
+
+ Directory of G:\SIMPLILEARN\COMPPLETED ASSINGMENTS\zomato tip
+
+04-10-2023  23:11    <DIR>          .
+04-10-2023  16:57    <DIR>          ..
+29-09-2023  14:07               256 ML_Zomato_rest_success_prediction_Shan_Singh.ipynb
+04-10-2023  22:21    <DIR>          ZOMATO-TIP
+04-10-2023  23:05               666 zomato.ipynb
+04-10-2023  23:11                 0 zomato.py
+10-12-2022  02:23       603,205,632 zomato_rawdata.sqlite
+28-09-2023  17:12                22 Zomato_resources.zip
+28-09-2023  17:12        95,748,536 _Da79856
+               6 File(s)    698,955,112 bytes
+               3 Dir(s)  331,034,959,872 bytes free
